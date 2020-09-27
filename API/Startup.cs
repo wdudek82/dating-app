14a7e2281732f1626410a5562412c948de890044
Wojciech Dudek <wdudek82@gmail.com>
2020-09-27 14:44:11 +0200

    Rename config and change from prop to field

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,12 +11,12 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly IConfiguration _config;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
