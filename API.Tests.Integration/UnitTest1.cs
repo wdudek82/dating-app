7160060c789d71e071cfd0b7b4969c9529dca641
Wojciech Dudek <wdudek82@gmail.com>
2020-09-27 20:45:27 +0200

    Add initial tests project (for integration tests)

---
+++ API.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using API.Entities;
+using RestSharp;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace API.Tests.Integration
+{
+    public class UnitTest1
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async void Test1()
+        {
+            // var client = new RestClient();
+            // var req = new RestRequest("https://localhost:5001/api/users");
+            // var res = await client.GetAsync<IEnumerable<AppUser>>(req);
+        }
+    }
+}
