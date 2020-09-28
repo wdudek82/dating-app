954774b2b35ca37ae1e2f9f5c71eea193c666194
Wojciech Dudek <wdudek82@gmail.com>
2020-09-28 17:24:55 +0200

    Section 3: Building a walking skeleton Part Two - Angular: add method for fetching users and show results in UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AppUser {
+  id: number;
+  userName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DatingApp';
+  appUsers: AppUser[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.http.get<AppUser[]>('https://localhost:5001/api/users').subscribe(
+      (users) => {
+        this.appUsers = users ?? [];
+      },
+      (error) => {
+        console.error(error);
+      },
+    );
+  }
 }
