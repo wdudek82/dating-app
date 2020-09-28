259d857fe5068d4df357567ef26ee0807e93991b
Wojciech Dudek <wdudek82@gmail.com>
2020-09-28 17:25:18 +0200

    Section 3: Building a walking skeleton Part Two - Angular: cleanup

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,16 +21,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddCors(options =>
-            // {
-            //     options.AddDefaultPolicy(
-            //         builder =>
-            //         {
-            //             builder.WithOrigins("*");
-            //             builder.WithMethods("GET");
-            //         });
-            // });
-
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
