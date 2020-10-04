047684e023ca622875a3a6bc14ae9a78ae8638bd
Wojciech Dudek <wdudek82@gmail.com>
2020-10-04 23:24:38 +0200

    Feature/section 4 authentication basics
    * Section 4: Authentication basics: add password and salt fields to AppUser and create migration
    
    * Section 4: Authentication basics: create base api controller and inherit from it in UserController
    
    * Section 4: Authentication basics: remove weatherforecast
    
    * Section 4: Authentication basics: add account controller with register endpoint
    
    * Section 4: Authentication basics: add RegisterDto
    
    * Section 4: Authentication basics: use dto in account register endpoint
    
    * Section 4: Authentication basics: add helper method to check if username is unique
    
    * Section 4: Authentication basics: add register dto validation
    
    * Section 4: Authentication basics: add login dto and login endpoint
    
    * Section 4: Authentication basics: add token service
    
    * Section 4: Authentication basics: add user DTO and return it's instance instead od user obj
    
    * Section 4: Authentication basics: add authorization middleware and additional auth attributes to UserController endpoints
    
    * Section 4: Authentication basics: extract application and identity services into separate extension classes
    
    * Section 4: Authentication basics: remove references to unused namespaces

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var userExists = await UserExists(registerDto.Username);
+            if (userExists)
+            {
+                return BadRequest("Username already exists.");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(u => u.UserName == loginDto.Username.ToLower().Trim());
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            var isDifferent = computeHash.Where((t, i) => t != user.PasswordHash[i]).Any();
+            if (isDifferent)
+            {
+                return Unauthorized("Invalid password");
+            }
+
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(
+                u => u.UserName == username.ToLower().Trim());
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,9 @@
-using API.Data;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -21,12 +19,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(_config);
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
+            services.AddCors();
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +47,7 @@ namespace API
                     .WithOrigins("https://localhost:4200");
             });
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
