72ea36e507e05c5b089c945cf36ce98e6d97c06e
Wojciech Dudek <wdudek82@gmail.com>
2020-10-15 22:27:01 +0200

    Section 5: Client login and register: add login and logout methods to account service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { AppUser } from "../nav/nav.component";
+import { Observable, ReplaySubject } from "rxjs";
+import { map } from "rxjs/operators";
+import { User } from "../_models/user";
 
 @Injectable({
   providedIn: "root",
 })
 export class AccountService {
   baseUrl = "https://localhost:5001/api";
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$: Observable<User> = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  login(model: object): Observable<void> {
+    return this.http.post<User>(`${this.baseUrl}/account/login`, model).pipe(
+      map((res) => {
+        const user = res;
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        // return res;
+      }),
+    );
   }
 
-  login(model: AppUser) {
-    return this.http.post(`${this.baseUrl}/account/login`, model);
+  setCurrentUser(user: User): void {
+    this.currentUserSource.next(user);
+  }
+
+  logout(): void {
+    localStorage.removeItem("user");
+    this.currentUserSource.next(null);
   }
 }
