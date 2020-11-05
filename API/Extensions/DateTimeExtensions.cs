f1314a667c121238fdb8c142897b126a41e31635
Wojciech Dudek <wdudek82@gmail.com>
2020-11-05 19:55:49 +0100

    Section 8: Extending the API: adding a DateTime extension to calculate age

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+
+            if (dob.Date > today.AddYears(-age))
+            {
+                age -= 1;
+            }
+
+            return age;
+        }
+    }
+}
