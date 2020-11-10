1e1e2007a8e7d9f833272ae5330e8887d82ff39d
Wojciech Dudek <wdudek82@gmail.com>
2020-11-10 20:47:36 +0100

    Section 8: Extending the API: add to user repository methods to fetch members (users mapped to member dto)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,29 +13,25 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepository.GetUsersAsync();
-            return Ok(users);
+            var members = await _userRepository.GetMembersAsync();
+            return Ok(members);
         }
 
         [HttpGet("{username}")]
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(username);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user);
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
