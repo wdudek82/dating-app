ba84a6478a607498adf3db00bed6c4816193a43d
Wojciech Dudek <wdudek82@gmail.com>
2020-11-10 20:53:51 +0100

    Section 8: Extending the API: use AutoMapper queryable extensions

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -37,9 +37,9 @@ namespace API.Entities
 
         public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
